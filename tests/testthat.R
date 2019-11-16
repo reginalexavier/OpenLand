70085e45b524739a4cfe0d2bc2bf47b7c8a41134
Reginal Exavier <redex03-mail@yahoo.com>
2019-11-16 17:24:22 -0400

    - pkgdown configurated

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(OpenLand)
+
+test_check("OpenLand")
---
+++ tests/testthat/test_demolandscape.R
@@ -0,0 +1,15 @@
+context("just testing for testing")
+
+
+test_that("whether the demo gives the same output", {
+
+  set.seed(159)
+
+  demo_raster <- demo_landscape(2000:2005, res = 1)
+
+  expect_equal(c(class(demo_raster[[1]])), "RasterLayer")
+
+  expect_equal(length(demo_raster), 6)
+
+
+})
